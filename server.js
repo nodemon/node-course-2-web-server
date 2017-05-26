e41da353659096e470ba6fe9e7530c3e65f128b7
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-05-26 15:14:38 -0400

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const hbs = require ('hbs');
 const fs = require ('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -66,6 +68,6 @@ app.get('/about', (req, res) => {
 
 
 // sercond param is a function which runs after listener starts
-app.listen(3000, ()=> {
-  console.log('server up on port 3000');
+app.listen(port, ()=> {
+  console.log(`server up on port ${port}`);
 });
